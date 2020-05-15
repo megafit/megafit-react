2d186a190b5248f7c6e70357f828e40ee79e17a6
prasetiodc <mprasetiodc@gmail.com>
2020-05-15 12:08:26 +0700

    change baseurl

--- src/config/API.js
+++ src/config/API.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const BaseURL = 'http://localhost:3000';
+// const BaseURL = 'http://localhost:3000';
 // const BaseURL = 'http://192.168.210.117:3000'; //localhost
-// const BaseURL = 'http://209.97.175.174:3000/'; //digitalocean
+const BaseURL = 'http://209.97.175.174:3000/'; //digitalocean
 
 const API = axios.create({
   baseURL: BaseURL
